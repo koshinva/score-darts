f6bb22b4a42ebc2b91c651780122dd2b6d7626b9
Koshin Valery <koshin.v@tomhunter.ru>
2024-12-17 14:45:52 +0300

    feat: layout card player

--- src/components/game.darts/ui/game.place.tsx
+++ src/components/game.darts/ui/game.place.tsx
@@ -5,7 +5,7 @@ export const GamePlace = () => {
   const players = useGameDartsStore((state) => state.players);
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
       {Object.keys(players).map((id) => (
         <PlayerCard key={id} id={id} />
       ))}
--- src/components/game.darts/ui/player.card.tsx
+++ src/components/game.darts/ui/player.card.tsx
@@ -1,4 +1,16 @@
+import { cn } from '@/lib/utils';
+import { useGameDartsStore } from '../model/game.darts.store';
 import { PlayerId } from '../types/player.game.types';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { Badge } from '@/components/ui/badge';
 
 type PlayerCardProps = {
   id: PlayerId;
@@ -7,5 +19,44 @@ type PlayerCardProps = {
 export const PlayerCard = (props: PlayerCardProps) => {
   const { id } = props;
 
-  return <div>{id}</div>;
+  const player = useGameDartsStore((state) => state.players[id]);
+
+  if (!player) {
+    return null;
+  }
+
+  return (
+    <Card>
+      <CardHeader className="text-center">
+        <CardTitle>{player.name}</CardTitle>
+        <CardDescription className="text-8xl font-semibold">{player.progress}</CardDescription>
+        <Separator />
+      </CardHeader>
+      <CardContent className="grid grid-cols-2 gap-2 p-4 pt-0">
+        <PlayerBadgeDetail title="среднее" value={player.avg.toFixed(2)} />
+        <PlayerBadgeDetail title="среднее (раунд)" value={player.legAvg.toFixed(2)} />
+        <PlayerBadgeDetail title="партии" value={player.legsWin} />
+        <PlayerBadgeDetail title="сеты" value={player.setsWin} />
+      </CardContent>
+      <CardFooter className="p-4 pt-0 flex justify-end">
+        <PlayerBadgeDetail title="последний ход" value={player.lastScore} className="text-md" />
+      </CardFooter>
+    </Card>
+  );
+};
+
+type PlayerBadgeDetailProps = {
+  title: string;
+  value: string | number;
+  className?: string;
+};
+
+const PlayerBadgeDetail = (props: PlayerBadgeDetailProps) => {
+  const { title, value, className } = props;
+
+  return (
+    <Badge variant="outline" className={cn('text-xs rounded-xl font-normal', className)}>
+      {title}:<span className="ml-1 font-bold">{value}</span>
+    </Badge>
+  );
 };
