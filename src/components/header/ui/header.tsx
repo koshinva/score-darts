ebfb58dd77e65ff4978295afab1853b5033e1ca9
Koshin Valery <koshin.v@tomhunter.ru>
2024-12-19 09:34:55 +0300

    feat: add confirm reset game

--- src/components/header/ui/header.tsx
+++ src/components/header/ui/header.tsx
@@ -1,12 +1,8 @@
-import { useGameDartsStore } from '@/components/game.darts';
-import { Button } from '@/components/ui/button';
-import { BoltIcon, LogOutIcon } from 'lucide-react';
+import { BoltIcon } from 'lucide-react';
 import { ToggleTheme } from './toggle.theme';
+import { ResetGame } from './reset.game';
 
 export const Header = () => {
-  const reset = useGameDartsStore((state) => state.reset);
-  const initialized = useGameDartsStore((state) => state.initialized);
-
   return (
     <header className="bg-secondary color-foreground px-8 py-2">
       <div className="flex items-center justify-between">
@@ -16,9 +12,7 @@ export const Header = () => {
         </div>
         <div className="flex items-center gap-2">
           <ToggleTheme />
-          <Button variant="outline" size="icon" onClick={reset} disabled={!initialized}>
-            <LogOutIcon />
-          </Button>
+          <ResetGame />
         </div>
       </div>
     </header>
---
+++ src/components/header/ui/reset.game.tsx
@@ -0,0 +1,52 @@
+import { useGameDartsStore } from '@/components/game.darts';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { LogOutIcon } from 'lucide-react';
+
+export const ResetGame = () => {
+  const reset = useGameDartsStore((state) => state.reset);
+  const initialized = useGameDartsStore((state) => state.initialized);
+
+  const onReset = () => {
+    reset();
+  };
+
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" size="icon" disabled={!initialized}>
+            <LogOutIcon />
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Уверены, что хотите сбросить игру?</DialogTitle>
+            <DialogDescription>
+              Если сбросите игру, то вы потеряете все данные текущего сеанса.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                Отменить
+              </Button>
+            </DialogClose>
+            <DialogClose>
+              <Button onClick={onReset}>Сбросить</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
