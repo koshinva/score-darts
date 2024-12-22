d6b62a8d7fae08e766bb6ecf5eb658fbab803956
Koshin Valery <koshin.v@tomhunter.ru>
2024-12-22 12:39:05 +0300

    feat: add hints for calculator

--- src/components/header/ui/header.tsx
+++ src/components/header/ui/header.tsx
@@ -1,8 +1,12 @@
 import { BoltIcon } from 'lucide-react';
 import { ToggleTheme } from './toggle.theme';
 import { ResetGame } from './reset.game';
+import { HintGame } from './hint.game';
+import { useGameDartsStore } from '@/components/game.darts';
 
 export const Header = () => {
+  const initialized = useGameDartsStore((state) => state.initialized);
+
   return (
     <header className="bg-secondary color-foreground px-8 py-2">
       <div className="flex items-center justify-between">
@@ -11,6 +15,7 @@ export const Header = () => {
           <span className="font-semibold text-lg">darts score</span>
         </div>
         <div className="flex items-center gap-2">
+          {initialized && <HintGame />}
           <ToggleTheme />
           <ResetGame />
         </div>
---
+++ src/components/header/ui/hint.game.tsx
@@ -0,0 +1,63 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Separator } from '@/components/ui/separator';
+import { cn } from '@/lib/utils';
+import {
+  BombIcon,
+  KeyboardIcon,
+  LightbulbIcon,
+  RadicalIcon,
+  SendHorizonalIcon,
+  UndoIcon,
+  XIcon,
+} from 'lucide-react';
+
+const hints = [
+  { icon: XIcon, description: 'Очистить текущий ход', className: 'bg-warning' },
+  { icon: BombIcon, description: 'Перебор в текущий ход', className: 'bg-warning' },
+  { icon: UndoIcon, description: 'Отменить последний ход', className: 'bg-destructive' },
+  { icon: SendHorizonalIcon, description: 'Отправить результат', className: 'bg-success' },
+  { icon: RadicalIcon, description: 'Переключить режим калькулятора', className: '' },
+  { icon: KeyboardIcon, description: 'Горячие клавиши (в разработке)', className: 'opacity-30' },
+];
+
+export const HintGame = () => {
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" size="icon">
+            <LightbulbIcon />
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogDescription>
+              <div className="flex flex-col gap-2">
+                {hints.map(({ icon: Icon, description, className }, index) => (
+                  <>
+                    <div key={index} className="flex items-center gap-2 text-sm">
+                      <div
+                        className={cn('flex items-center justify-center p-1 rounded-sm', className)}
+                      >
+                        <Icon className="stroke-primary" />
+                      </div>
+                      <p>{description}</p>
+                    </div>
+                    {index !== hints.length - 1 && <Separator />}
+                  </>
+                ))}
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
