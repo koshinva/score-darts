1514bd03ba8f1f5d86cc60df7056e0a65ea1bc6c
Koshin Valery <koshin.v@tomhunter.ru>
2024-12-22 12:11:00 +0300

    feat: edit styles of radio group start form game

--- src/components/game.form/ui/game.type.group.tsx
+++ src/components/game.form/ui/game.type.group.tsx
@@ -20,15 +20,17 @@ export const GameTypeGroup = () => {
             <RadioGroup
               onValueChange={field.onChange}
               defaultValue={field.value}
-              className="flex gap-6"
+              className="flex gap-2 items-center"
             >
               {gameType.map((type) => {
                 return (
                   <FormItem key={type} className="flex items-center gap-1">
                     <FormControl>
-                      <RadioGroupItem value={type} />
+                      <RadioGroupItem value={type} className="peer hidden" />
                     </FormControl>
-                    <FormLabel className="font-normal">{type}</FormLabel>
+                    <FormLabel className="cursor-pointer rounded-sm border border-border px-4 py-2 font-normal shadow-sm hover:bg-muted peer-aria-checked:bg-muted peer-aria-checked:ring-1 peer-aria-checked:ring-ring/50">
+                      {type}
+                    </FormLabel>
                   </FormItem>
                 );
               })}
